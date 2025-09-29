67bd595679ee8c2bff4a853ed001b3cc9b33f437
Steeve-feuille-Ciseaux <libertybanshee1@gmail.com>
2025-09-29 08:51:31 +0200

    v0.07.1

--- quickAdd/club/urls.py
+++ quickAdd/club/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ajouter_etudiant, name='ajouter_etudiant'),
-    path('liste/', views.liste_etudiants, name='liste_etudiants'),
+    path('ajout/', views.ajouter_etudiant, name='ajouter_etudiant'),  
+    path('', views.liste_etudiants, name='liste_etudiants'),          
     path('export-word/', views.export_word, name='export_word'),
 ]
